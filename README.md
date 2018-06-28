ac6520e7d46457ef22e82a210d9cf43b5c6f65fd
Robert1211 <maculewicz.au@gmail.com>
2018-06-29 00:55:35 +1000

    project0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tic Tac Toe
+### name:project0
+
+### Link to the live Site
+ https://robert1211.github.io/project0/
+------------------------
+
+This Game has been developed to use the Characters 'O' and 'X'to represent players so users need not create players.Include a reset button so that when a game ends, the board can be cleared and a new game can begin.
+----
+Use only: JavaScript, jQuery, HTML, CSS, Atom
+
+1. Create a file on desktop
+2. Write pseudo code
+3. Planing how logic working in Tic Tac Toe
+4. Planing all function
+
+Feature:
+- Game for two players;
+- Display who is 'WIN' or 'DRAW';
+- Reset button;
--- js/main.js
+++ js/main.js
@@ -1,108 +1,114 @@
-let player1=""
-let player2=""
+let player1 = ""
+let player2 = ""
 let turn = "";
 let playerOneIsNext = true;
-
+ //create array//
 const grid = [
-    ['','',''],
-    ['','',''],
-    ['','','']
-  ];
-let Winner ='';
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+//declere varibles for Winer,MoveCount//
+let Winner = '';
 let MoveCount = 0;
+//create function with logic about who win or loss;//
+const winnerCheck = function(player) {
+  if((grid[0][0] === grid[0][1]) && (grid[0][0] === grid[0][2]) && (grid[0][0] === player) ||
+    (grid[1][0] === grid[1][1]) && (grid[1][0] === grid[1][2]) && (grid[1][0] === player) ||
+    (grid[2][0] === grid[2][1]) && (grid[2][0] === grid[2][2]) && (grid[2][0] === player) ||
 
-const winnerCheck = function(player){
-  if( (grid[0][0] === grid[0][1]) && (grid[0][0] === grid[0][2]) && (grid[0][0] === player) ||
-      (grid[1][0] === grid[1][1]) && (grid[1][0] === grid[1][2]) && (grid[1][0] === player) ||
-      (grid[2][0] === grid[2][1]) && (grid[2][0] === grid[2][2]) && (grid[2][0] === player) ||
 
-
-     (grid[0][0] === grid[1][0]) && (grid[0][0] === grid[2][0]) && (grid[0][0] === player) ||
-     (grid[0][1] === grid[1][1]) && (grid[0][1] === grid[2][1]) && (grid[0][1] === player) ||
-     (grid[0][2] === grid[1][2]) && (grid[0][2] === grid[2][2]) && (grid[0][2] === player) ||
+    (grid[0][0] === grid[1][0]) && (grid[0][0] === grid[2][0]) && (grid[0][0] === player) ||
+    (grid[0][1] === grid[1][1]) && (grid[0][1] === grid[2][1]) && (grid[0][1] === player) ||
+    (grid[0][2] === grid[1][2]) && (grid[0][2] === grid[2][2]) && (grid[0][2] === player) ||
 
 
     (grid[0][0] === grid[1][1]) && (grid[0][0] === grid[2][2]) && (grid[0][0] === player) ||
-    (grid[0][2] === grid[1][1]) && (grid[0][2] === grid[2][0]) && (grid[0][2] === player) )
-    {
-      boardMsg(player + " is winner!");
-      Winner = player;
-      MoveCount=0;
-      $('body').addClass('gameOver');         //add new class name for winner
-      setTimeout(function () {
-        $('body').removeClass('gameOver');
-      }, 900);    //change time
-      return true;
-    } else if(MoveCount === 8 ){
-
-     boardMsg('Draw')
-     $('body').addClass('gameOver');           //add new class name for Draw
-     setTimeout(function () {
-       $('body').removeClass('gameOver');    //revove class comeback to old background
-      }, 900);
-     return false;
-   }
-
+    (grid[0][2] === grid[1][1]) && (grid[0][2] === grid[2][0]) && (grid[0][2] === player)) {
+    boardMsg(player + " is winner!");
+    Winner = player;
+    MoveCount = 0;
+    //function for change img background with setTimeout
+    //add new class gameOver name
+    $('body').addClass('gameOver');
+    setTimeout(function() {
+      $('body').removeClass('gameOver');
+    }, 900);
+    //if is true show in boardMsg whow is winner//
+    return true;
+    //add new conditional for DRAW
+    //  MoveCount = 0;                              //dodane
+  } else if (MoveCount === 8) {
+    boardMsg('Draw');
+    return false;
+    //add function for firs add after remove class in change img background//
+    $('body').addClass('gameOver');
+    setTimeout(function() {
+    $('body').removeClass('gameOver');
+    }, 900);
+    return false;
+  }
 };
-// function make arey empty any time//
-const resetF = function(){
- for (var i = 0; i < grid.length; i++) {
-   let innerArray = grid[i]
-     for (var j = 0; j < innerArray.length; j++) {
+ // create function for refresh browser loop insaid loop//
+  const resetF = function() {
+    for (var i = 0; i < grid.length; i++) {
+    let innerArray = grid[i]
+    for (var j = 0; j < innerArray.length; j++) {
       innerArray[j] = ""
     }
-       $('.col').text('');       //give me empty storge
-       $('#result').text('');    //give me empty storge
-
-    }
+    //print in html empty string for all marks 'X' and 'Y'
+    $('.col').text('');
+    //print in html empty string for result in display
+    $('#result').text('');
+      MoveCount = 0;        //Move = 0
+  }
 };
-
-$("#reset").click(function(){      // call for function reset
-  resetF();
+    // call for function reset//
+    $("#reset").click(function() {
+    resetF();
 });
-
-const playTurn = function (row, column) {
-
-  // console.log(row, column);
-  if ( playerOneIsNext === true ) {    // place an "X" into the grid[num]
+//create function for playTurn ('who is next')//
+  const playTurn = function(row, column) {
+    // place an "X" into the grid[num]
+    if (playerOneIsNext === true) {
     grid[row][column] = 'X';
-     const result = winnerCheck("X");
-      if (result === true) {
-    }
+  const result = winnerCheck("X");
+    if (result === true) {}
     playerOneIsNext = false;
-
-    }else if ( playerOneIsNext === false ){  // playerOneIsNext = false
-     grid[row][column] = 'O';               // place an "O" into the grid[num]
-      winnerCheck("O")
-       playerOneIsNext = true                 // checkForDraw()
-   }
-    MoveCount = MoveCount + 1;
+    // playerOneIsNext = false
+  } else if (playerOneIsNext === false) {
+    // place an "O" into the grid[num]
+    grid[row][column] = 'O';
+    winnerCheck("O");   //ad const result
+    playerOneIsNext = true
+  }
+      MoveCount = MoveCount + 1;
+      //testing play turn in console;
       console.log(MoveCount);
 };
 
-  function boardMsg(X){
+  function boardMsg(X) {
   return $("#result").html(X); //replace html on 'text'
 };
 
-    $(".col").click(function (){
-    // if this square is already occupied then return and don't place a new piece here.
-    if ( $(this).text() !== "" ) {
-      return;
-    }
-     if (playerOneIsNext) {
-       $(this).text("X")
+$(".col").click(function() {
+  // if this square is already occupied then return and don't place a new piece here.
+    if ($(this).text() !== "") {
+    return;
+  }
+    if (playerOneIsNext) {
+    $(this).text("X")
 
-     } else{
-       $(this).text("O")
-     }
-     let row = $(this).parent().index() - 2; // -2 to ignore the <button> and the bg that is in the game
-     let column = $(this).index();
-     playTurn(row, column);
+  } else {
+    $(this).text("O")
+  }
+  let row = $(this).parent().index() - 2; // -2 to ignore the <button> and the bg that is in the game
+  let column = $(this).index();
+    playTurn(row, column);
+});
 
-    });
-
-
- //another version
+////////////////////////////////////////////////////////////////////////////////
+//another version for function conect with HTML
 /*
 $('#0').click( function () {
   if (playerOneIsNext) {
